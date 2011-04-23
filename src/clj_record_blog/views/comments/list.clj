a181b13ac60a742c689070e607fde79ce84226c6
Marcelo Giorgi <marklazz.uy@gmail.com>
2011-04-23 15:04:43 -0300

    Removed unused views from comments

--- src/clj_record_blog/views/comments/list.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-record-blog.views.comments.index
-  (:use [compojure.core]
-        [clj-record-blog.views.layouts.application]
-        [hiccup.core]))
-
-(defn render [] (layout {:main (default-main) :sidebar (default-sidebar)}))

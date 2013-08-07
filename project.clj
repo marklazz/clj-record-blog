a6f09cb63ec301739ca7f449872ab2d9b3856641
Marcelo Giorgi <marklazz.uy@gmail.com>
2013-08-06 23:12:57 -0300

    Use resources dir instead of static to make it work with latest versions

--- project.clj
+++ project.clj
@@ -5,11 +5,8 @@
                  [mysql/mysql-connector-java "5.1.25"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
-                 ;[ring "1.1.1"]
-                 ;[clj-haml "0.2.1-SNAPSHOT"]
                  [clj-json "0.5.3"]
                  [clj-record "1.1.4"]]
   :plugins [[lein-ring "0.7.1"]]
-  ;:dev-dependencies [[lein-ring "0.4.0"]]
-  :resources-path "static"
+  :resources-path "resources"
   :ring {:handler clj-record-blog.core/app})
